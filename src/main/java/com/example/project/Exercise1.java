296e4d058cbf606cc86b906b9163639fb7870d69
allaiquec <88158031+allaiquec@users.noreply.github.com>
2022-05-16 21:43:01 -0500

    nuevo1

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -13,7 +13,7 @@ public class Exercise1 {
 			System.out.println(obj.esCuadradoPerfecto(n));			
 		}
 	}
-
+	
 	//Si el numero es 1, pues defrente es cuadrado perfecto
 	public boolean esCuadradoPerfecto(int numero) {
 		if(numero == 1) 
@@ -24,14 +24,13 @@ public class Exercise1 {
 	}
 	
 	public boolean esCuadradoPerfecto(int numero,int i) {
-		if(numero ==i)     //Si recorrimos todos los números, y aún no encontramos alguno que cumpliera, será false
+		if(numero ==i)     //Si recorrimos todos los numeros, y aun no encontramos alguno que cumpliera sera false
 			return false;
 		
-		if ((numero % i == 0) && (numero / i == i))  // Veremos que si cumple con la siguientes condiciones será un cuadrado perfecto
+		if ((numero % i == 0) && (numero / i == i))  // Veremos que si cumple con la siguientes condiciones sera un cuadrado perfecto
              return true;
          
 		return esCuadradoPerfecto(numero,i+1);  //Incrementamos al i
 	}
 }
 
-}
